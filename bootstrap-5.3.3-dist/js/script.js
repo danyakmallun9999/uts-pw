463434c232f2cfeb6dde56c4c074cd73dfdebb7d
danyakmallun9999 <danyclasher9999@gmail.com>
2024-10-23 08:36:58 +0700

    menambahkan fitur animasi anime.js

---
+++ bootstrap-5.3.3-dist/js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    anime({
+        targets: '.logo',
+        opacity: [0, 1],
+        translateX: ['100%', '0%'],
+        duration: 1000,
+        easing: 'easeInOutQuad',
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    anime({
+        targets: '.hero-text',
+        opacity: [0, 1],
+        translateX: ['-100%', '0%'],
+        duration: 1000,
+        easing: 'easeInOutQuad',
+    });
+});
